e9ce3ce85fb17bf9c4f1923093de48a60c7604d6
zhaoxin <zhaoxin@9fbank.cc>
2019-09-20 21:38:01 +0800

    Updates

---
+++ precache-manifest.3d3d28a5fc41d6833329abc561485555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a28b0a7821a64d31d6d177f0f79887b",
+    "url": "/autoread/index.html"
+  },
+  {
+    "revision": "8f0e41d674e04b3fb2eb",
+    "url": "/autoread/static/css/2.3dcec901.chunk.css"
+  },
+  {
+    "revision": "b435833f232258f629a0",
+    "url": "/autoread/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8f0e41d674e04b3fb2eb",
+    "url": "/autoread/static/js/2.2c017c38.chunk.js"
+  },
+  {
+    "revision": "b435833f232258f629a0",
+    "url": "/autoread/static/js/main.3e693750.chunk.js"
+  },
+  {
+    "revision": "34502921a6b766988c8b",
+    "url": "/autoread/static/js/runtime-main.ebe1b770.js"
+  }
+]);
